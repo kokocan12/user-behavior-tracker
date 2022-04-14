7998e0f70acf536c74964014cadaa91521a26bb0
dlgudwns <dlgudwnsl1234@gmail.com>
2022-04-14 18:04:03 +0900

    e2e test tool 추가

---
+++ cypress/integration/keyboard-event.test.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+context('keyboard event', () => {
+  beforeEach(() => {
+    cy.demoPage();
+  });
+
+  it('cypress test', () => {
+    expect(true).equal(true);
+  });
+});

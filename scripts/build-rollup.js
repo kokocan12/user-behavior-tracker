501424e14bd96546a831305d406b6c5776ae02d8
kokocan12 <sky203614@gmail.com>
2022-04-19 00:43:15 +0900

    rollup babel plugin 추가

--- scripts/build-rollup.js
+++ scripts/build-rollup.js
@@ -1,7 +1,7 @@
 const exec = require('exec-sh').promise;
 
 async function buildRollup() {
-  await exec('rollup dist/esm/index.js --file dist/bundle.js --format iife --name Tracker');
+  await exec('rollup --config rollup.config.js');
 }
 
 module.exports = {

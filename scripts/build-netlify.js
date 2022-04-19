825ac7db128cc4e86ee83908dbabda56867860fa
kokocan12 <sky203614@gmail.com>
2022-04-19 11:06:03 +0900

    netlify build script 수정

--- scripts/build-netlify.js
+++ scripts/build-netlify.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra');
 async function buildForNetlify() {
   await exec('yarn build');
   await exec('parcel build demo/index.html --out-dir demo-build');
-  fs.copy('demo/_redirects', 'demo-build/public/_redirects');
+  fs.copy('demo/_redirects', 'demo-build/_redirects');
 }
 
 buildForNetlify();

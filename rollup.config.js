cc988e7bd90574f2a711ae09a960049f3bc1af4f
kokocan12 <sky203614@gmail.com>
2022-04-21 12:56:53 +0900

    rollup terser plugin 적용

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'dist/esm/index.js',
@@ -9,5 +10,5 @@ export default {
     name: 'Tracker',
     strict: false,
   },
-  plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
+  plugins: [resolve(), babel({ babelHelpers: 'bundled' }), terser()],
 };
